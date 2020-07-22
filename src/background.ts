c3474b47237d2db21afebac32160339342fee60c
Masakazu <m.nomura.733@nitech.jp>
2020-07-22 09:16:37 +0900

    first commit

---
+++ src/background.ts
@@ -0,0 +1,50 @@
+chrome.webRequest.onHeadersReceived.addListener(
+    function(details) {
+        for (let i = 0; i < details.responseHeaders!.length; ++i) {
+            if (details.responseHeaders![i].name.toLowerCase() == 'x-frame-options') {
+                details.responseHeaders!.splice(i, 1);
+                return {
+                    responseHeaders: details.responseHeaders
+                };
+            }
+        }
+    }, {
+        urls: ["<all_urls>"]
+    }, ["blocking", "responseHeaders"]
+);
+
+// エンターが押された時
+chrome.omnibox.onInputEntered.addListener(
+    function(query) {
+        let search_url = chrome.runtime.getURL("index.html");
+        search_url += "?q=" + query;
+        navigate(search_url);
+    }
+);
+
+function navigate(url: string) {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        const id = tabs[0].id!
+        chrome.tabs.update(id, {url: url});
+    });
+}
+
+// ペインに検索クエリが入力され，エンターが押された前
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details)
+    { 
+        console.log("DEBUG!!")
+        // 適切なパラメータかチェック
+        if (details.url.indexOf("https://example.com?q=") != -1
+            || details.url.indexOf("chrome-extension://" + chrome.runtime.id + "/?q=") != -1) {
+
+            const query = details.url.split("/?q=")[1]
+            let search_url = chrome.runtime.getURL("index.html");
+            search_url += "?q=" + query;
+            
+            return {redirectUrl: search_url};
+        }
+    },
+    {urls: ["<all_urls>"]},
+    ["blocking"]
+);
---
+++ src/content_scripts.ts
@@ -0,0 +1,25 @@
+/**
+ * Thanks
+ * 基本的なアルゴリズムの参照（主な変更点は，1. Bing->Yahoo! 2. Purlの廃止 3. TypeScriptの導入）
+ * https://github.com/zmarty/double-shot-search
+ * TypeScriptでChrome拡張の開発環境の構築
+ * https://qiita.com/markey/items/ea9ed18a1a243b39e06e?utm_campaign=popular_items&utm_medium=feed&utm_source=popular_items
+ * ミニマムなChrome拡張の開発手法
+ * https://qiita.com/Ancient_Scapes/items/822409167ae3a0b76dbe
+ */
+import $ from 'jquery';
+
+$(document).ready(function () {
+    const left = "https://search.yahoo.co.jp/search?p=";
+    const right = "https://www.google.com/search?q=";
+
+    let query = location.href.split("index.html?")[1].split("q=")[1]
+
+    if (query === undefined) {
+        query = ""
+    }
+
+    $("#left").attr("src", left + query);
+    $("#right").attr("src", right + query);
+
+});
